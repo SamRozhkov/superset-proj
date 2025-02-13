0ffa38e0e4438e85095ef658e6dba4a5122c3ddf
Sam <sam.rojkov@gmail.com>
2025-02-13 18:39:31 +0500

    Add files via upload

--- docker/pythonpath_dev/superset_config_docker.py
+++ docker/pythonpath_dev/superset_config_docker.py
@@ -1,12 +1,20 @@
 import os
-from typing import Optional
+from typing import Optional, Literal
+from datetime import timedelta
 
-#from superset.stats_logger import StatsdStatsLogger
+from superset.stats_logger import StatsdStatsLogger
 
-from flask_appbuilder.security.manager import AUTH_LDAP, AUTH_DB
+from flask_appbuilder.security.manager import AUTH_LDAP, AUTH_DB, AUTH_OAUTH
 from celery.schedules import crontab
 from cachelib.redis import RedisCache
 from dateutil import tz
+
+from flask import Flask, redirect, g, request
+from flask_appbuilder import expose, IndexView
+from superset.superset_typing import FlaskResponse
+from superset.utils.core import (
+    get_user_id,
+)
 
 SMTP_HOST = os.environ.get('SMTP_HOST')
 SMTP_STARTTLS = os.environ.get('SMTP_STARTTLS')
@@ -40,8 +48,6 @@ WEBDRIVER_OPTION_ARGS = [
     "--disable-extensions",
 ]
 
-WEBDRIVER_BASEURL = 'http://localhost:8088/'  # env('DASHBOARDS_BASE_URL')
-
 SCREENSHOT_LOCATE_WAIT = 1000
 SCREENSHOT_LOAD_WAIT = 6000
 
@@ -51,10 +57,29 @@ THUMBNAIL_SELENIUM_USER = "admin"
 
 BABEL_DEFAULT_LOCALE = "ru"
 
+SHOW_STACKTRACE = True
+PROFILING = True
+
 LANGUAGES = {
     "ru": {"flag": "ru", "name": "Russian"},
-    # "en": {"flag": "us", "name": "English"},
+    "en": {"flag": "us", "name": "English"},
 }
+
+D3_FORMAT = {
+     "decimal": ",",           # - decimal place string (e.g., ".").
+     "thousands": "\u00a0",         # - group separator string (e.g., ",").
+     "grouping": [3],          # - array of group sizes (e.g., [3]), cycled as needed.
+     "currency": ["", "\u00a0\u20bd"]     # - currency prefix/suffix strings (e.g., ["$", ""])
+}
+
+CURRENCIES = ["USD", "EUR", "GBP", "INR", "MXN", "JPY", "CNY"]
+
+D3_FORMAT_PREFIXIES = {
+  "k": 'тыс.',
+  "M": 'млн',
+  "G": 'млрд'
+}
+
 
 FEATURE_FLAGS = {
     "ALERT_REPORTS": True,
@@ -67,9 +92,9 @@ FEATURE_FLAGS = {
     "ENABLE_TEMPLATE_PROCESSING": True,
     "ENABLE_TEMPLATE_REMOVE_FILTERS": True,
     "DASHBOARD_CACHE": True,
-    "UX_BETA": False,
+    "UX_BETA": True,
     "TAGGING_SYSTEM": True,
-    # "GLOBAL_ASYNC_QUERIES": True,
+    #"GLOBAL_ASYNC_QUERIES": True,
     "DASHBOARD_NATIVE_FILTERS_SET": True,
     "DASHBOARD_FILTERS_EXPERIMENTAL": True,
     "RLS_IN_SQLLAB": True,
@@ -78,51 +103,89 @@ FEATURE_FLAGS = {
     "HORIZONTAL_FILTER_BAR": True,
     "RLS_FORM_QUERY_REL_FIELDS": True,
     "DASHBOARD_EDIT_CHART_IN_NEW_TAB": True,
+    "DRILL_BY": True,
+    "CACHE_QUERY_BY_USER": True,
     #"EMBEDDABLE_CHARTS": True,
+    "PLAYWRIGHT_REPORTS_AND_THUMBNAILS": True,
+    "CHART_PLUGINS_EXPERIMENTAL": True,
 }
 
 HTML_SANITIZATION = False
 
-MAPBOX_API_KEY = ''
+#SECRET_KEY = 'wtreAEzlsuEVZu/S2B3I8XnTyx4CqJkoxw4tyrK+HRk1JrE1bBWAuVeU'
 
-#APP_ICON = "/static/assets/images/superset-logo-horiz-beta.png"
+MAPBOX_API_KEY = "pk.eyJ1IjoiaXRhcHBhcmF0IiwiYSI6ImNsMXVhOXJwMTA4YnczY21tczdmNG41c28ifQ.rNjd1zZfhPo0vpcon8kc9w"
+
+APP_ICON = "/static/assets/images/group_24.png"
 
 DRUID_TZ = tz.gettz('Asia/Yekaterinburg')
 
 #AUTH_TYPE = AUTH_LDAP
-# AUTH_TYPE = AUTH_DB
+#AUTH_TYPE = AUTH_DB
+#AUTH_TYPE = AUTH_OAUTH
 
-AUTH_LDAP_SERVER = "ldap://org.com"
+#PUBLIC_ROLE_LIKE='public'
+
+AUTH_LDAP_SERVER = "ldap://192.168.0.200"
 AUTH_USER_REGISTRATION = True
 AUTH_USER_REGISTRATION_ROLE = "Public"
-AUTH_LDAP_BIND_USER = "CN=superset,DC=org,DC=com"
-AUTH_LDAP_SEARCH = "OU=superset,DC=org,DC=com"
-# AUTH_LDAP_UID_FIELD = "sAMAccountName"
-AUTH_LDAP_UID_FIELD = "mail"
+RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
+#f"reCAPTCHA_{os.environ.get('RECAPTCHA_PUBLIC_KEY')}"
+
+AUTH_LDAP_BIND_USER = "UID=superset,CN=users,CN=accounts,DC=home,DC=local"
+AUTH_LDAP_SEARCH = "cn=users,cn=accounts,dc=home,dc=local"
+    #"CN=users,CN=accounts,DC=home,DC=local"
+#AUTH_LDAP_UID_FIELD = "sAMAccountName"
+AUTH_LDAP_UID_FIELD = "uid"
 AUTH_LDAP_FIRSTNAME_FIELD = "givenName"
 AUTH_LDAP_LASTNAME_FIELD = "sn"
 AUTH_LDAP_EMAIL_FIELD = "mail"
-AUTH_LDAP_BIND_PASSWORD = os.environ.get("AUTH_LDAP_BIND_PASSWORD")
+AUTH_LDAP_BIND_PASSWORD = "superset" #os.environ.get("AUTH_LDAP_BIND_PASSWORD")
 
 # AUTH_LDAP_USERNAME_FORMAT = ""
 # AUTH_LDAP_SEARCH_FILTER = ""
 AUTH_ROLE_ADMIN = 'Admin'
 AUTH_ROLE_PUBLIC = 'Public'
-PUBLIC_ROLE_LIKE = 'Public'
 # AUTH_USER_REGISTRATION_ROLE = "Public_LDAP"
 
-AUTH_ROLES_MAPPING = {
-    "CN=Пользователи,DC=org,DC=com": [
-        "User"],
-    "CN=supersetadmin,DC=org,DC=com": [
-        "Admin"],
-}
+#AUTH_ROLES_MAPPING = {
+#    "CN=Пользователи,OU=Groups,OU=Аппарат Губернатора ЯНАО,OU=ИОГВ,DC=yg,DC=loc": [
+#        "User"],
+#    "CN=supersetadmin,OU=Groups,OU=Аппарат Губернатора ЯНАО,OU=ИОГВ,DC=yg,DC=loc": [
+#        "Admin"],
+#}
 AUTH_LDAP_GROUP_FIELD = "memberOf"
-# AUTH_ROLES_SYNC_AT_LOGIN = True
-PERMANENT_SESSION_LIFETIME = 1800
+AUTH_ROLES_SYNC_AT_LOGIN = False
+#PERMANENT_SESSION_LIFETIME = 1800
 
-REDIS_HOST = 'redis'
-    #'10.12.4.253'
+#-------------- OAUTH -----------------#
+
+AUTH_USER_REGISTRATION_ROLE = "Gamma"
+LOGOUT_REDIRECT_URL='http://192.168.0.201:8080/realms/superset/protocol/openid-connect/logout'
+# OAuth provider configuration for Keycloak
+OAUTH_PROVIDERS = [
+  {
+    'name': 'keycloak',
+    'icon': 'fa-key',
+    'token_key': 'access_token', # Keycloak uses 'access_token' for the access token
+    'remote_app': {
+      'client_id': 'superset-ui',
+      'client_secret': 'GFCWqktlceYz5Cuza1Ct6Yg1iAQ6VO0T',
+      'client_kwargs': {
+        'scope': 'openid profile email',
+      },
+      'server_metadata_url': 'http://192.168.0.26:28080/realms/superset/.well-known/openid-configuration',
+      'api_base_url': 'http://192.168.0.26:28080/realms/superset/protocol/',
+      #'server_metadata_url': 'http://192.168.0.201:8080/realms/superset/.well-known/openid-configuration',
+      #'api_base_url': 'http://192.168.0.201:8080/realms/superset/protocol/',
+    },
+  }
+  ]
+
+
+#--------------------------------------#
+
+REDIS_HOST = 'redis'#'10.12.4.253'
 REDIS_PORT = 6379
 
 CACHE_CONFIG = {
@@ -183,7 +246,8 @@ class CeleryConfig(object):
     imports = (
         'superset.sql_lab',
         'superset.tasks',
-        'superset.tasks.thumbnails'
+        'superset.tasks.thumbnails',
+        'superset.tasks.cache',
     )
     result_backend = CACHE_CONFIG['CACHE_REDIS_URL']  # 'redis://localhost:6379/0'
     worker_log_level = 'DEBUG'
@@ -200,11 +264,12 @@ class CeleryConfig(object):
             'ignore_result': True,
         }
     }
+
     beat_schedule = {
-         #'email_reports.schedule_hourly': {
-         #   'task': 'email_reports.schedule_hourly',
-         #   'schedule': crontab(minute='1', hour='*'),
-         #},
+        # 'email_reports.schedule_hourly': {
+        #    'task': 'email_reports.schedule_hourly',
+        #    'schedule': crontab(minute='1', hour='*'),
+        # },
         "reports.prune_log": {
             "task": "reports.prune_log",
             "schedule": crontab(minute=10, hour=0),
@@ -215,7 +280,7 @@ class CeleryConfig(object):
         },
         'cache-warmup-hourly': {
             'task': 'cache-warmup',
-            'schedule': crontab(minute="*/20", hour='*'),
+            'schedule': crontab(minute="*/10", hour='*'),
             'kwargs': {
                 'strategy_name': 'top_n_dashboards',
                 'top_n': 5,
@@ -258,6 +323,9 @@ TALISMAN_CONFIG = {
     }
 }
 
+#
+# Добавление цветовых схем в суперсет. Требует перезапуска контейнеров.
+#
 EXTRA_CATEGORICAL_COLOR_SCHEMES = [
     {
         "id": 'Yamal AUTODOR',
@@ -266,8 +334,62 @@ EXTRA_CATEGORICAL_COLOR_SCHEMES = [
         "isDefault": False,
         "colors": ['#1C1D1C', '#F26635', '#4D4E4D', '#EF8050', '#777777', '#F4A27C',
                    '#A4A4A4', '#F8BEA4']
-        # "colors":['#1C1D1C', '#4D4E4D', '#777777', '#A4A4A4', '#F26635', '#EF8050', '#F4A27C', '#F8BEA4']
+    },{
+        "id": 'ATK Yamal',
+        "description": '',
+        "label": 'ATK Yamal Colors',
+        "isDefault": False,
+        "colors": ['#8DD7F7', '#C8C7C7', '#0065A3', '#0095CA', '#3A4859', '#E03838']
     }]
 
 
-#STATS_LOGGER = StatsdStatsLogger(host='10.31.21.17', port=2015, prefix='superset')
+# Global async query config options.
+# Requires GLOBAL_ASYNC_QUERIES feature flag to be enabled.
+
+GLOBAL_ASYNC_QUERIES_REDIS_CONFIG = {
+    "port": 6379,
+    "host": "redis",
+    "password": "",
+    "db": 2,
+    "ssl": False,
+}
+
+GLOBAL_ASYNC_QUERIES_REDIS_STREAM_PREFIX = "async-events-"
+GLOBAL_ASYNC_QUERIES_REDIS_STREAM_LIMIT = 1000
+GLOBAL_ASYNC_QUERIES_REDIS_STREAM_LIMIT_FIREHOSE = 1000000
+GLOBAL_ASYNC_QUERIES_JWT_COOKIE_NAME = "async-token"
+GLOBAL_ASYNC_QUERIES_JWT_COOKIE_SECURE = False
+GLOBAL_ASYNC_QUERIES_JWT_COOKIE_SAMESITE: Optional[
+    Literal["None", "Lax", "Strict"]
+] = None
+GLOBAL_ASYNC_QUERIES_JWT_COOKIE_DOMAIN = None
+GLOBAL_ASYNC_QUERIES_JWT_SECRET = "sdcp471J60KDgN6p8dOqieTtqUDaBiwFnd59+JJYLK7vbaBMEo86kpOi"
+GLOBAL_ASYNC_QUERIES_TRANSPORT = "polling"
+    #"ws"
+GLOBAL_ASYNC_QUERIES_POLLING_DELAY = int(
+    timedelta(milliseconds=500).total_seconds() * 1000
+)
+GLOBAL_ASYNC_QUERIES_WEBSOCKET_URL = "ws://127.0.0.1:8080/"
+
+
+#class SupersetDashboardIndexView(IndexView):
+#    @expose("/")
+#    def index(self) -> FlaskResponse:
+#        return redirect("/dashboard/list/")
+#FAB_INDEX_VIEW = f"{SupersetDashboardIndexView.__module__}.{SupersetDashboardIndexView.__name__}"
+
+
+class SupersetIndexView(IndexView):
+    @expose("/")
+    def index(self) -> FlaskResponse:
+        if not g.user or not get_user_id():
+            # Do steps for anonymous user e.g.
+            return redirect("/login")
+        # Do steps for authenticated user e.g.
+        return redirect("/dashboard/list")
+
+
+FAB_INDEX_VIEW = f"{SupersetIndexView.__module__}.{SupersetIndexView.__name__}"
+
+
+#STATS_LOGGER = StatsdStatsLogger(host='graphite', port=8125, prefix='superset')
